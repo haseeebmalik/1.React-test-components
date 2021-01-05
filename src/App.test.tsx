04ce85491908414456f837fcf28392f69d46ef4b
haseeebmalik <m.haseebmalik123@gmail.com>
2021-01-06 01:15:34 +0500

    added travis.yml

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,9 +1,15 @@
-import React from 'react';
-import { render, screen } from '@testing-library/react';
-import App from './App';
+import React from "react";
+import { render, screen } from "@testing-library/react";
+import App from "./App";
 
-test('renders learn react link', () => {
+test("renders hello world", () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
+  const linkElement = screen.getByText(/hello world/i);
+  expect(linkElement).toBeInTheDocument();
+});
+
+test("renders haseeb", () => {
+  render(<App />);
+  const linkElement = screen.getByText(/haseeb/i);
   expect(linkElement).toBeInTheDocument();
 });
