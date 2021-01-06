08d1514d16b528dbfcfedba6ebc2468bfc2e60bc
haseeebmalik <m.haseebmalik123@gmail.com>
2021-01-06 22:24:59 +0500

    some test updates

--- src/About.test.tsx
+++ src/About.test.tsx
@@ -4,6 +4,9 @@ import About from "./About";
 
 test("renders About comp.", () => {
   render(<About />);
-  const linkElement = screen.getByText(/hello in about/i);
-  expect(linkElement).toBeInTheDocument();
+  const linkElement1 = screen.getByText(/hello in about/i);
+  expect(linkElement1).toBeInTheDocument();
+
+  const linkElement2 = screen.getByPlaceholderText("Username");
+  expect(linkElement2).toBeInTheDocument();
 });
--- src/About.tsx
+++ src/About.tsx
@@ -6,7 +6,7 @@ function About() {
   return (
     <div className="App">
       <h1>hello in about</h1>
-      <input type="text" />
+      <input type="text" placeholder="Username" />
     </div>
   );
 }
